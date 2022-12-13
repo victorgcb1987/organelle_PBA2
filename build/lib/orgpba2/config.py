f38180f00d59a4a7c3931ecd8588221168159245
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2022-12-13 11:40:50 +0100

    Delete build/lib/orgpba2 directory

--- build/lib/orgpba2/config.py
+++
@@ -1,5 +0,0 @@
-BINARIES_REQUIREMENTS = {"minimap2": {"binaries": ["minimap2"],
-                                     "user_path": "MINIMAP2_PATH"},
-                         "blast": {"binaries": ["blastn", "makeblastdb"],
-                                   "user_path": "BLAST_PATH"},
-                                   }
