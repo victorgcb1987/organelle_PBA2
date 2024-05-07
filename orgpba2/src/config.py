c98a90e2aa569c6730590ed8f95396abf491fecd
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2024-05-07 15:19:35 +0200

    config added for flye

--- orgpba2/src/config.py
+++ orgpba2/src/config.py
@@ -17,7 +17,9 @@ EXECUTABLES_REQUIREMENTS = {"minimap2": {"executable": "minimap2",
                             "racon": {"executable": "racon",
                                       "user_path": "RACON_PATH"},
                             "nucmer": {"executable": "nucmer",
-                                      "user_path": "NUCMER_PATH"}}
+                                      "user_path": "NUCMER_PATH"},
+                            "flye": {"executable": "flye",
+                                      "user_path": "FLYE_PATH"}}
 
 MAGIC_NUMS_COMPRESSED = [b'\x1f\x8b\x08', b'\x42\x5a\x68', 
                          b'\x50\x4b\x03\x04']
@@ -27,6 +29,7 @@ OUTPUT_FOLDERS = {"haplotypes": "07_haplotypes",
                   "mapped_seqs": "02_mapped_seqs_seqtk_filtlong",
                   "subsampled_seqs": "02_mapped_seqs_seqtk_filtlong",
                   "canu": "03_assembly_canu",
+                  "flye": "03_assembly_flye",
                   "blast_db": "00_blasts",
                   "polished_assembly": "04_polished_assembly_racon",
                   "curated_assembly": "06_curated_assembly_mummer",
@@ -46,7 +49,8 @@ OUTPUT_FILENAMES = {"haplotypes": "06_haplotypes.fasta",
                     "mapping_for_polishing": "04_polishing_mapping_iteration_{}.paf",
                     "mapping_for_haplotypes": "07_haplotypes_reads_mapped.paf",
                     "oriented_contigs": "oriented_contigs.fasta",
-                    "no_redundant": "05_non_redundant.fasta"}
+                    "no_redundant": "05_non_redundant.fasta",
+                    "flye": "assembly.fasta"}
 
 COMPLEMENTARY_NUCLEOTIDE = {"A": "T", "T": "A", 
                             "C": "G", "G": "C", 
